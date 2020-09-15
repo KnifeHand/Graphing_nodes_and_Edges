92b126bcc6b00e8aa748424575114691c7fc1735
Matt Hurt <mhurt@msudenver.edu>
2020-09-15 09:54:24 -0600

    Update comments of how SyntaxAnalyzer and Tree work.  FIXME issue with addOne(). Adding HW3 python example.

--- activity_07_syntax_analyzer/src/Tree.scala
+++ activity_07_syntax_analyzer/src/Tree.scala
@@ -2,16 +2,29 @@
  * CS3210 - Principles of Programming Languages - Fall 2020
  * Instructor: Thyago Mota
  * Description: Activity 07 - Tree
+ *
+ *
  */
 
 import scala.collection.mutable.ArrayBuffer
 
+//Simple implementation of a Tree
 class Tree(var label: String) {
 
   private val branches: ArrayBuffer[Tree] = new ArrayBuffer[Tree]
+  // Allows you to add branches
+  def add(branch: Tree): Unit = branches.addOne(branch) // FIXME: addOne is not a value member?
 
-  def add(branch: Tree): Unit = branches.addOne(branch)
-
+  // print the tree horizontally in a text format
+  /*
+  expected outcome of parse stream:
+  A
+    ab1
+        abc1
+        abc2
+    ab32
+    abc3
+   */
   private def print(current: Tree, tabs: String): String = {
     var out = ""
     if (current == null)
@@ -28,19 +41,22 @@ class Tree(var label: String) {
 }
 
 // example code
+// How to create a tree.  Each tree node has a label
 object Tree {
   def main(args: Array[String]): Unit = {
-    val tree = new Tree("A")
+    // each branch is a tree by itself
+    val tree = new Tree("A") // "A" is going to be the root
     val ab1 = new Tree("ab1")
     val ab2 = new Tree("ab2")
     val ab3 = new Tree("ab3")
     val abc1 = new Tree("abc1")
     val abc2 = new Tree("abc2")
-    tree.add(ab1)
+    // How to connect all the nodes.
+    tree.add(ab1) // label of branch
     tree.add(ab2)
     tree.add(ab3)
-    ab1.add(abc1)
-    ab1.add(abc2)
+    ab1.add(abc1) // subbranch from ab1
+    ab1.add(abc2) // subbranch from ab1
     print(tree)
   }
 }
