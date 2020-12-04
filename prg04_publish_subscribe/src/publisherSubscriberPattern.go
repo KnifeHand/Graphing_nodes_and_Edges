00e10856a038ed01f4a63ed49999ee3709180f1f
Matt Hurt <mhurt@msudenver.edu>
2020-12-03 22:28:04 -0700

    Changes made to the publishers, publisher, subscriber, subscribers.  Should print but doesn't.

--- prg04_publish_subscribe/src/publisherSubscriberPattern.go
+++ prg04_publish_subscribe/src/publisherSubscriberPattern.go
@@ -16,140 +16,116 @@ import (
 
 // PubSub is our struct for the publisher-subscriber pattern
 type PubSub struct {
+	mu     sync.Mutex
 	topics map[string][]chan string
-	mu     sync.RWMutex
 }
-
- var ps = &PubSub{make(map[string][]chan string),
-		sync.RWMutex{}}
-
 
 var wg sync.WaitGroup
 
-// TODO: creates and returns a new channel on a given topic, updating the PubSub struct
-func (ps *PubSub) subscribe(topic string) []chan string {
-	//defer wg.Done()
-	//ps.mu.Lock()
-
-	var tmp []chan string
-	if ps.topics[topic] != nil {
-		ps.topics[topic] = tmp
-	} else {
-		ps.topics[topic] = tmp
-	}
-	go printer(topic, tmp)											// added print
-	//ps.mu.Unlock()
-	return tmp
-}
-
 // TODO: writes the given message on all the channels associated with the given topic
 func (ps PubSub) publish(topic string, msg string) {
-	//defer wg.Done()
-	ps.mu.RLock()
-
-	for  mapped, associated := range ps.topics {
-		if mapped == topic {
-			for _, channel := range associated{
-				go printer(topic, associated)                         // extra print
-				channel <- msg
-			}
-		}
-		}
+	ps.mu.Lock()
+	//defer ps.mu.Unlock()
+	for _, ch := range ps.topics[topic] {
+		//ps.topics[topic] = append(ps.topics[topic], ch)
+		go func(chl chan string) {
+			chl <- msg
+		}(ch)
 	}
-
 	ps.mu.Unlock()
-}
-
-func (ps PubSub) _publishGo(channel []chan string, msg string){
-	for _, channel :=  range channel {
-		channel <- msg
-	}
 }
 
 // TODO: sends messages taken from a given array of message, one at a time and at random intervals, to all topic subscribers
-func (ps *PubSub) publisher( topic string, msgs [5]string) {
-	//defer wg.Done()
-	ps.mu.RLock()
-
-		for message := range msgs {
-			go ps.publish(topic, msgs[message])
-			time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-		}
-	ps.mu.Unlock()
+func publisher(ps PubSub, topic string, msgs []string) {
+	//ps.mu.Lock()
+	//defer ps.mu.Unlock()
+	time.Sleep(5 * time.Second)
+	for _, message := range msgs {
+		//ps.publish(topic, msgs[message])
+		time.Sleep(time.Duration(rand.Intn(7)) * time.Second)
+		ps.publish(topic, message)
+	}
+	wg.Done()
 }
 
+// TODO: creates and returns a new channel on a given topic, updating the PubSub struct
+func (ps PubSub) subscribe(topic string) <-chan string {
+	ps.mu.Lock()
+	ch := make(chan string)
+	ps.topics[topic] = append(ps.topics[topic], ch)
+	ps.mu.Unlock()
+	return ch
+}
 
 // TODO: reads and displays all messages received from a particular topic
-func (ps *PubSub) subscriber(name string, topic string) {
-	//defer wg.Done()
-	ps.mu.Lock()
+func subscriber(ps PubSub, name string, topic string) {
 
-	for topic, slice := range ps.topics {
-		go printer(topic, slice)
-	}
-	ps.mu.Unlock()
-}
-
-func printer(topic string, channel []chan string,) {
-	for item := range channel {
-		var receive = <-channel[item]
-		fmt.Printf("Topic: %s; Message: %s\n", topic, receive)
+	ch := ps.subscribe(topic)
+	for {
+		if data, confirm := <-ch; confirm {
+			fmt.Println(name + "message received: " + data)
+		} else {
+			break
+		}
 	}
 }
 
-//func printer(name string, topic string, channel []chan string,) {
+//func printer(topic string, channel []chan string,) {
 //	for item := range channel {
 //		var receive = <-channel[item]
-//		fmt.Printf("Name: %s; Topic: %s; Message: %s\n",name, topic, receive)
+//		fmt.Printf("Topic: %s; Message: %s\n", topic, receive)
 //	}
 //}
-
 
 func main() {
 
 	// TODO: create the ps struct
-
-
+	ps := PubSub{topics: make(map[string][]chan string)}
 
 	// TODO: create the arrays of messages to be sent on each topic
-	var beesArray [5]string
+	beesArray := []string{
+		"bees are pollinators",
+		"bees are pollinators",
+		"all worker bees are female",
+		"bees have 5 eyes",
+		"bees fly about 20mph",
+	}
 
-	beesArray[0] = "bees are pollinators"
-	beesArray[1] = "bees produce honey"
-	beesArray[2] = "all worker bees are female"
-	beesArray[3] = "bees have 5 eyes"
-	beesArray[4] = "bees fly about 20mph"
+	//beesArray[0] = "bees are pollinators"
+	//beesArray[1] = "bees produce honey"
+	//beesArray[2] = "all worker bees are female"
+	//beesArray[3] = "bees have 5 eyes"
+	//beesArray[4] = "bees fly about 20mph"
 
-	var philosophyArray [5]string
+	philosophyArray := []string{
+		"And if you gaze for long into an abyss, the abyss gazes also into you.",
+		"Dreams are the touchstones of our characters.",
+		"The question is not what you look at, but what you see.",
+		"Go confidently in the direction of your dreams! Live the life you’ve imagined.",
+		"Rather than love, than money, than fame, give me truth.",
+	}
 
-	philosophyArray[0] = "And if you gaze for long into an abyss, the abyss gazes also into you."
-	philosophyArray[1] = "Dreams are the touchstones of our characters."
-	philosophyArray[2] = "The question is not what you look at, but what you see."
-	philosophyArray[3] = "Go confidently in the direction of your dreams! Live the life you’ve imagined."
-	philosophyArray[4] = "Rather than love, than money, than fame, give me truth."
+	//philosophyArray[0] = "And if you gaze for long into an abyss, the abyss gazes also into you."
+	//philosophyArray[1] = "Dreams are the touchstones of our characters."
+	//philosophyArray[2] = "The question is not what you look at, but what you see."
+	//philosophyArray[3] = "Go confidently in the direction of your dreams! Live the life you’ve imagined."
+	//philosophyArray[4] = "Rather than love, than money, than fame, give me truth."
 
 	// TODO: set wait group to 2 (# of publishers)
-	//wg.Add(2)
-
-
-
+	wg.Add(2)
 
 	// TODO: create the publisher goroutines
 
-	go ps.publisher("bees", beesArray)
-	go ps.publisher("philosophy", philosophyArray)
-
+	go publisher(ps, "bees facts", beesArray)
+	go publisher(ps, "philosophy", philosophyArray)
 
 	// TODO: create the subscriber goroutines
-	go ps.subscriber("nature","bees")
-	go ps.subscriber("humanities", "philosophy")
-
-
+	go subscriber(ps, "broham", "bee facts")
+	go subscriber(ps, "mama", "philosophy quotes")
+	go subscriber(ps, "dingus", "philosophy quotes")
 
 	// TODO: wait for all publishers to be done
 
-	//wg.Wait()
-
-
+	wg.Wait()
 
 }
